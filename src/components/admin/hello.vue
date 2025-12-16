509ad1ce9346cd0eedd4274485376ad4306b0556
zhaojingnan <2026617199@qq.com>
2025-12-16 13:07:13 +0800

    开始重构，移除Element UI的样式，移除Tailwind CSS，重新写

--- src/components/admin/hello.vue
+++
@@ -1,8 +0,0 @@
-<!-- 打印你好 -->
-<template>
-  <div class="hello-container">
-    <h1 class="hello-title">Hello, Admin!</h1>
-    <p class="hello-description">Welcome to the admin dashboard.</p>
-  </div>
-</template>
-<script setup></script>
--- src/main.js
+++ src/main.js
@@ -2,25 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-// import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'element-plus/dist/index.css'
-import './style/style.css'
-
 // 创建Vue应用
 const app = createApp(App)
 
 // 使用Pinia状态管理
 const pinia = createPinia()
 app.use(pinia)
-
-// 使用Element Plus
-// app.use(ElementPlus)
-
-// 全局注册所有图标
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 
 // 使用路由
 app.use(router)
